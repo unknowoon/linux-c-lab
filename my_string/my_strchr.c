3a1e7f299b9d10c0f704f0188b7d20dc2ce5c342
unknowoon <wgb1223@gmail.com>
2025-04-27 21:58:38 +0900

    feat: strchr

---
+++ my_string/my_strchr.c
@@ -0,0 +1,26 @@
+#include "my_string.h"
+#include <string.h>
+
+char *my_strchr(const char *cs, int c) {
+    while (*cs) {
+        if (*cs++ == (char) c) {
+            return --cs;
+        }
+    }
+
+    return NULL;
+}
+
+int main ()
+{
+  char str[] = "This is a sample string";
+  char * pch;
+  printf ("Looking for the 's' character in \"%s\"...\n",str);
+  pch=my_strchr(str,'s');
+  while (pch!=NULL)
+  {
+    printf ("found at %d\n",pch-str+1);
+    pch=my_strchr(pch+1,'s');
+  }
+  return 0;
+}
