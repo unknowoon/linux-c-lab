e7d42a2e8ab6105846e163996f18d5d3e08c51d5
rnasterwoo <wgb1223@gmail.com>
2025-04-28 14:49:28 +0900

    feat: strspn

---
+++ my_string/my_strspn.c
@@ -0,0 +1,33 @@
+#include "my_string.h"
+#include <string.h>
+
+size_t my_strspn(const char *str1, const char *str2) {
+	long count = 0;
+
+
+	while (*str1) {
+		int pos = (int) *str1++;
+		char *buf = str2;
+		while (*buf) {
+			if (pos == (int) *buf++) {
+				count++;
+				break;
+			}
+		}
+		if (*buf == '\0') {
+			return (size_t) count;
+		}
+	}
+	return (size_t) count;
+}
+
+int main ()
+{
+  int i;
+  char strtext[] = "1t29th";
+  char cset[] = "1234567890";
+
+  i = my_strspn (strtext,cset);
+  printf ("The initial number has %d digits.\n",i);
+  return 0;
+}
